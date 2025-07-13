9dd39a40f9cf30c0f2267c3f4c2f9ef7310a717c
ashuk <ashuk@ASHU>
2025-07-13 16:19:17 +0800

    ssss

--- Mock.HospitalityService/Program.cs
+++ Mock.HospitalityService/Program.cs
@@ -3,11 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-//Add Yarp for reverse proxy
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
